08e419bd545c678489c6ba3149be6cf525af2f63
Paps90 <barra90@gmail.com>
2021-08-29 14:07:59 +0200

    Test push Gitbhub
    Test: prima di questo push funzionava tutto

---
+++ app/src/main/java/com/example/itelinushealthcheck/Next_Activity.java
@@ -0,0 +1,14 @@
+package com.example.itelinushealthcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Next_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
